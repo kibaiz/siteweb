83f272ea17a78432792424f4eeb1105a291d40ed
Kibaiz <jeanbaptiste1rasolofo@gmail.com>
2023-03-31 12:49:34 +0200

    4th commit

---
+++ src/Contact.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Contact=() =>{
+return (<>
+  <h3>Hello Contact</h3>
+  </>);
+}
+
+export default Contact;
---
+++ src/Service.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Service=() =>{
+return (<>
+  <h3>Hello Service</h3>
+  </>);
+}
+
+export default Service;
